8baa45ffb32cb3acbbe7afb2136fcd3d82b9cdbc
Laura <lmcneil2@gmu.edu>
2013-04-27 18:26:36 -0400

    added java script images
    added java script images

---
+++ final/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+
+$('#slideshow').cycle({ 
+    fx:'scrollLeft',  
+    fit:0,
+    slideResize:1,
+    timeout:0,
+    next:'#slideshow' //change to button div
+
+});
+
+});
+
+
--- javascript-examples-master/scripts.js
+++ javascript-examples-master/scripts.js
@@ -1,23 +1,9 @@
 
 $(document).ready(function() {
-	//alert("I'm tired");
+	
 
-	//Set a variable to a string.
-	var firstName = 'Laura';
-
-	//Set a variale for last name
-	var lastName = 'McNeil';
-
-	//Set a variable to an integer.
-	var age = 29;
-
-	//alert ('My name is ' + firstName +' '+ lastName + '.');
-	//alert ('My age is '+ age + '.' + ' Next year I will be ' + (age + 1));
-
-$('#image-slideshow').cycle({
-	 fx:      'scrollRight', 
-    next:   '#image-slideshow', 
-    timeout:  0, 
+$('#image-slideshow').cycle({ 
+    fx:'scrollLeft',  
 
 });
 
